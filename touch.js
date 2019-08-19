6dc524030273a67bf07921324b9b84c76a26d4ce
Roman Brito <romanbrito1@gmail.com>
2019-08-18 22:42:15 -0500

    slideshow and click

--- touch.js
+++ touch.js
@@ -7,9 +7,30 @@
 //     if (e.target.id === 'todd') e.target.classList.add('hide');
 // });
 
-var DELEGATE = document.querySelector('body');
+var DELEGATE = document.querySelector("body");
 
-DELEGATE.addEventListener('click', function(e) {
-    console.log(e.target);
-    if (e.target.id === 'deli-menu') DELEGATE.classList.add('show-deli');
+DELEGATE.addEventListener("click", function(e) {
+  console.log(e.target);
+  if (e.target.id === "full-service")
+    DELEGATE.classList.add("show-full-service");
 });
+
+var slideIndex = 0;
+// carousel(".top-carousel li", 2000);
+carousel(".bottom-carousel li", 2500);
+
+function carousel(myClass, interval) {
+  var i;
+  var x = document.querySelectorAll(myClass);
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(function() {
+    carousel(myClass, interval);
+  }, interval); 
+}
