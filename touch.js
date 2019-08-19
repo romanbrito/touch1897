81ce68471199f6875ff34e2d2899f141712eb9ed
Roman Brito <roman.brito@osmsolutions.com>
2019-08-19 10:52:43 -0500

    using less styles

--- touch.js
+++ touch.js
@@ -1,42 +1,13 @@
-var DELEGATE = document.querySelector("body");
-
-DELEGATE.addEventListener("click", function(e) {
-  if (e.target.id === "full-service")
-    DELEGATE.classList.add("show-full-service");
-  var fullServiceTimeOut = setTimeout(function() {
-    DELEGATE.classList.remove("show-full-service");
-  }, 5000);
-});
-
-// var slideIndex = 0;
-// carousel(".top-carousel li", 2000);
-// carousel(".bottom-carousel li", 2500);
-
-// function carousel(myClass, interval) {
-//   var i;
-//   var x = document.querySelectorAll(myClass);
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {
-//     slideIndex = 1;
-//   }
-//   x[slideIndex - 1].style.display = "block";
-//   setTimeout(function() {
-//     carousel(myClass, interval);
-//   }, interval);
-// }
-
+// carousel object
 function carouselObj(myClass, interval) {
-  this.myClass = myClass;
-  this.interval = interval;
-
-  this.slideIndex = 0;
-}
+    this.myClass = myClass;
+    this.interval = interval;
+  
+    this.slideIndex = 0;
+  }
 
 carouselObj.prototype.carousel = function() {
-    var self = this;
+  var self = this;
 
   function slideShow() {
     var i;
@@ -53,15 +24,37 @@ carouselObj.prototype.carousel = function() {
     setTimeout(function() {
       slideShow();
     }, self.interval);
-
   }
 
   slideShow();
-
 };
 
+// run carousels
 var bottomCarousel = new carouselObj(".bottom-carousel li", 2500);
 bottomCarousel.carousel();
 
 var topCarousel = new carouselObj(".top-carousel li", 2000);
 topCarousel.carousel();
+
+// var whatIs1897 = new carouselObj(".what-is-1897 li", 1000);
+// whatIs1897.carousel();
+
+// click events
+var DELEGATE = document.querySelector("body");
+
+DELEGATE.addEventListener("click", function(e) {
+    var showClass = 'show-' + e.target.id
+    // main-buttons-second
+  if (e.target.id === "what-is-1897") DELEGATE.classList.add(showClass);
+  if (e.target.id === "qsr") DELEGATE.classList.add("show-full-service");
+  if (e.target.id === "gng") DELEGATE.classList.add("show-full-service");
+
+  // main buttons-first
+  if (e.target.id === "full-service") DELEGATE.classList.add(showClass);
+  if (e.target.id === "qsr") DELEGATE.classList.add("show-full-service");
+  if (e.target.id === "gng") DELEGATE.classList.add("show-full-service");
+
+  var fullServiceTimeOut = setTimeout(function() {
+    DELEGATE.classList.remove(showClass);
+  }, 5000);
+});
